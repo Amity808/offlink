a20402bbbce24f7c371f8ed6b5e02b85e682c6a3
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-10-27 19:23:08 +0100

    Feat: add useContractWrite

---
+++ frontend/src/app/hooks/useContractWrite.js
@@ -0,0 +1,24 @@
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import offlick from "../../app/contract/offlink.json"
+import { BigNumber } from "ethers";
+
+export const useContractSendWrite = (functionName, args) => {
+    const gasLimit = BigNumber.from(1000000)
+    const { config } = usePrepareContractWrite({
+        address: offlick.address,
+        abi: offlick.abi,
+        functionName: functionName,
+        args,
+        overrides: {
+            gasLimit
+        },
+        onError: (err) => {
+            console.log(err);
+            
+        }
+    })
+    const { data, isSuccess, write, writeAsync, error, isLoading} = useContractWrite(config)
+
+    return { data, isSuccess, write, writeAsync, isLoading}
+
+}
