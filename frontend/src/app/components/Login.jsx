3f10d530415c6a661d65d90d78054b2e99de6ff6
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-11-05 11:46:30 +0100

    fix bug

--- frontend/src/app/components/Login.jsx
+++ frontend/src/app/components/Login.jsx
@@ -40,76 +40,76 @@ const Login = () => {
   return (
     <>
       {/* <!--md:flex-col--> */}
-      <div class="main flex flex-col md:flex md:flex-row lg:flex-row lg:flex min-h-screen	w-full">
+      <div className="main flex flex-col md:flex md:flex-row lg:flex-row lg:flex min-h-screen	w-full">
         {/* <!--main1 start--> */}
         <div
-          class="main1 md:w-[30%] lg:w-[40%] w-full 
+          className="main1 md:w-[30%] lg:w-[40%] w-full 
          md:flex md:flex-row lg:flex-row md:items-center
          md:justify-center lg:items-center lg:justify-center lg:flex max-sm:hidden"
         >
           <div
-            class="main11 lg:space-x-4 md:space-x-2 space-x-2
+            className="main11 lg:space-x-4 md:space-x-2 space-x-2
            w-full lg:flex lg:items-center lg:justify-center md:flex
             md:items-center md:justify-center "
           >
             {/* <img src="/img/Group1.png" alt="logo img" /> */}
-            <h1 class="logo-txt md:text-3xl lg:text-4xl flex items-center">
+            <h1 className="logo-txt md:text-3xl lg:text-4xl flex items-center">
               OFF LINK{" "}
             </h1>
           </div>
         </div>
         {/* <!--main1 ends--> */}
 
-        <div class="main2 flex items-center justify-center lg:w-[60%] md:w-[70%] w-full max-sm:mx-auto">
-          <div class="main2-txt md:w-[85%] lg:w-[50%]">
+        <div className="main2 flex items-center justify-center lg:w-[60%] md:w-[70%] w-full max-sm:mx-auto">
+          <div className="main2-txt md:w-[85%] lg:w-[50%]">
             <div>
               <div>
-                <p class="wel h-[10%] lg:text-5xl md:text-4xl font-bold flex items-center lg:hidden md:hidden sm:block max-sm:mt-[30px]">
+                <p className="wel h-[10%] lg:text-5xl md:text-4xl font-bold flex items-center lg:hidden md:hidden sm:block max-sm:mt-[30px]">
                   OFFLINK
                 </p>
-                <p class="wel h-[10%] lg:text-3xl md:text-2xl font-semibold flex items-center">
+                <p className="wel h-[10%] lg:text-3xl md:text-2xl font-semibold flex items-center">
                   Login Your Account
                 </p>
               </div>
             </div>
 
             <form action="" onSubmit={handleLogin}>
-              <div class="he-mail w-full h-[25%]  cursor-pointer ">
-                <label class="em font-medium ">EMAIL OR USERNAME</label>
+              <div className="he-mail w-full h-[25%]  cursor-pointer ">
+                <label className="em font-medium ">EMAIL OR USERNAME</label>
                 <input
                   type="email"
                   placeholder="offlink@gmail.com"
                   name="email"
                   for="pass"
-                  class="email1 w-full cursor-pointer bg-transparent border-b-2 border-black py-2 placeholder-rgba(0, 0, 0, 0.40)-500"
+                  className="email1 w-full cursor-pointer bg-transparent border-b-2 border-black py-2 placeholder-rgba(0, 0, 0, 0.40)-500"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
-                <div class="relative">
+                <div className="relative">
                   <input
                     type="password"
                     for="email"
                     placeholder="Password"
                     name="pass"
-                    class="pass1 w-full cursor-pointer my-5 bg-transparent border-b-2 border-black py-2 placeholder-rgba(0, 0, 0, 0.40)-500"
+                    className="pass1 w-full cursor-pointer my-5 bg-transparent border-b-2 border-black py-2 placeholder-rgba(0, 0, 0, 0.40)-500"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
               </div>
 
-              <div class="rem flex space-x-0  items-center justify-between font-medium  h-[10%]">
-                {/* <div class="for cursor-pointer">FORGOT PASSWORD</div> */}
+              <div className="rem flex space-x-0  items-center justify-between font-medium  h-[10%]">
+                {/* <div className="for cursor-pointer">FORGOT PASSWORD</div> */}
               </div>
-              <button class="flex w-full h-14 justify-center items-center font-medium cursor-pointer login">
+              <button className="flex w-full h-14 justify-center items-center font-medium cursor-pointer login">
                 Login
               </button>
             </form>
 
-            <div class="dont flex  w-full h-8 items-center font-medium">
+            <div className="dont flex  w-full h-8 items-center font-medium">
               {" "}
               <div>Don&apos;t have an account</div> &nbsp;
-              <div class="res cursor-pointer my-2">
+              <div className="res cursor-pointer my-2">
                 <Link href="/signup">SignUp</Link>
               </div>
             </div>
